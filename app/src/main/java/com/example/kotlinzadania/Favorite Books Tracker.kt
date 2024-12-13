da1b8707ed781a97d92036ab4887601c59b4c766
igor <igor.kowalczyk07@poczta.fm>
2024-12-13 22:28:02 +0100

    created task 3

--- app/src/main/java/com/example/kotlinzadania/Favorite Books Tracker.kt
+++ app/src/main/java/com/example/kotlinzadania/Favorite Books Tracker.kt
@@ -1,31 +1,45 @@
-class dodajKsiazke{
+class Program {
+    val ksiazki = mutableListOf("")
+
     fun dodaj() {
-        while(true) {
-            println("podaj nazwe ksiazi, autora, oraz rok wydania: ")
+        while (true) {
+            println("\npodaj nazwe ksiazki, autora, oraz rok wydania, lub wpisz 'koniec' aby zakonczyc dodawanie: ")
             val ksiazka = readln()
 
-            if(ksiazka == "end"){
+            if (ksiazka.lowercase() == "koniec") {
                 break
-            }
-            else {
-                val ksiazki = mutableListOf(
-                    "Harry Potter i Kamień Filozoficzny - autor: J.K. Rowling, rok wydania: 2002"
-                )
+            } else {
                 ksiazki.add(ksiazka)
                 println("Twoja lista zawiera ksiazki: " + ksiazki)
+                sortuj()
             }
         }
     }
 
-    fun filtruj(){
+    fun filtrujRok() {
+        println("Podaj rok z ktorego maja byc ksiazki")
+        val rok = readln()
+        println(ksiazki.filter { rKsiazki -> rKsiazki.contains(rok) })
 
     }
 
+    fun filtrujAutora() {
+        println("Podaj jakiego autora maja byc ksiazki")
+        val autor = readln().lowercase()
+        println(ksiazki.filter { aKsiazki -> aKsiazki.contains(autor) })
+    }
+
+    fun sortuj() {
+        ksiazki.sortBy { it.substringBefore(",").trim() }
+        println("Posortowane książki alfabetycznie po tytule: $ksiazki")
+    }
 }
 
-
-
 fun main(){
-    val dodajks = dodajKsiazke()
-    dodajks.dodaj()
+    val program = Program()
+
+    program.dodaj()
+    program.filtrujAutora()
+    program.filtrujRok()
+    program.sortuj()
 }
