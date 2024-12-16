8c150d8641ac2dd33c8cd32f1fa9b8ff94740655
Damian <damiankontakt7@gmail.com>
2024-12-16 21:47:36 +0100

    Dodanie funkcji wynikPowyzejProgu

--- app/src/main/java/com/example/kotlinzadania/Sports Results Analyzer.kt
+++ app/src/main/java/com/example/kotlinzadania/Sports Results Analyzer.kt
@@ -3,7 +3,7 @@ class sportsResultAnalyzer{
     var wyniki = mutableListOf<Int>()
 
     fun dodajWynik(){
-        println("Podaj wynik meczu: ")
+        println("Podaj wynik meczu:")
         val wynik = readln().toIntOrNull()
         if(wynik != null){
             wyniki.add(wynik)
@@ -14,13 +14,26 @@ class sportsResultAnalyzer{
         }
     }
 
+    fun wynikPowyzejProgu(){
+        println("Podaj próg:")
+        val prog = readln().toIntOrNull()
+        if(prog != null) {
+            val powyzejProgu = wyniki.filter { it > prog }
+            println("Wyniki powyżej $prog: $powyzejProgu")
+        }else{
+            println("Błąd!")
+        }
+    }
+
     fun menu(){
         while (true){
             println("\nWybierz opcję:")
             println("1. Dodaj wynik meczu")
+            println("2. Pokaż wynik powyżej progu")
 
             when(readln().toIntOrNull()) {
                 1 -> dodajWynik()
+                2 -> wynikPowyzejProgu()
 
                 else -> println("Błąd!")
             }
