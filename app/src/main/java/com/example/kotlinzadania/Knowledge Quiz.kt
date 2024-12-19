2e78e977a1583da392094834aaed2fc0190df6e7
Damian <damiankontakt7@gmail.com>
2024-12-19 21:57:57 +0100

    Dodanie klasy PrawdaFalsz i funkcji main

--- app/src/main/java/com/example/kotlinzadania/Knowledge Quiz.kt
+++ app/src/main/java/com/example/kotlinzadania/Knowledge Quiz.kt
@@ -3,3 +3,15 @@ interface Pytanie {
     fun sprawdzOdpowiedz(odpowiedz: String): Boolean
 }
 
+class PrawdaFalsz(val pytanie: String, val poprawnaOdpowiedz: Boolean) : Pytanie {
+    override fun zapytaj() = "$pytanie (prawda/fałsz)"
+    override fun sprawdzOdpowiedz(odpowiedz: String) =
+        (odpowiedz == "prawda" && poprawnaOdpowiedz) || (odpowiedz == "fałsz" && !poprawnaOdpowiedz)
+}
+
+fun main() {
+    val pytanie = PrawdaFalsz("Czy Lewandowski jest najlepszym piłkarzem na świecie?", true)
+    println(pytanie.zapytaj())
+    val odpowiedz = readLine() ?: ""
+    println(if (pytanie.sprawdzOdpowiedz(odpowiedz)) "Poprawna odpowiedź!" else "Błędna odpowiedź.")
+}
