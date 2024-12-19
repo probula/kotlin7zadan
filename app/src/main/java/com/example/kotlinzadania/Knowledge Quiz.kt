c4b33f19a6dd32a26bd2e59afd9382235e42d932
Damian <damiankontakt7@gmail.com>
2024-12-19 22:10:24 +0100

    Dodanie klasy WielkorotnyWybor i zmodyfikowanie funkcji main

--- app/src/main/java/com/example/kotlinzadania/Knowledge Quiz.kt
+++ app/src/main/java/com/example/kotlinzadania/Knowledge Quiz.kt
@@ -9,9 +9,21 @@ class PrawdaFalsz(val pytanie: String, val poprawnaOdpowiedz: Boolean) : Pytanie
         (odpowiedz == "prawda" && poprawnaOdpowiedz) || (odpowiedz == "fałsz" && !poprawnaOdpowiedz)
 }
 
+class WielokrotnyWybor(val pytanie: String, val opcje: List<String>, val indeksOdpowiedzi: Int) : Pytanie {
+    override fun zapytaj() = "$pytanie\n" + opcje.joinToString("\n")
+    override fun sprawdzOdpowiedz(odpowiedz: String) =
+        odpowiedz.toIntOrNull() == indeksOdpowiedzi
+}
+
 fun main() {
-    val pytanie = PrawdaFalsz("Czy Lewandowski jest najlepszym piłkarzem na świecie?", true)
-    println(pytanie.zapytaj())
-    val odpowiedz = readLine() ?: ""
-    println(if (pytanie.sprawdzOdpowiedz(odpowiedz)) "Poprawna odpowiedź!" else "Błędna odpowiedź.")
+    val pytania = listOf(
+        PrawdaFalsz("Czy Lewandowski jest najlepszym piłkarzem na świecie?", true),
+        WielokrotnyWybor("Ile goli strzelił Lewandowski w ciągu 9 min podczas meczu Bayernu Monachium przeciwko Wolfsburgowi w 2015 roku?",
+            listOf("1. 5", "2. 1", "3. 4", "4. 3"), 1),
+    )
+    for (p in pytania) {
+        println(p.zapytaj())
+        val odpowiedz = readLine() ?: ""
+        println(if (p.sprawdzOdpowiedz(odpowiedz)) "Poprawna odpowiedź!" else "Błędna odpowiedź.")
+    }
 }
